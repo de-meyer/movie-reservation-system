2c3dcabf69b647de3adba3aa8ade5b06ea43f418
de-meyer <dmeyer.private@gmail.com>
2025-03-29 17:46:10 +0100

    Add user security conf

---
+++ src/main/kotlin/com/cli/fancy/movie_reservation_system/api/user/UserSecurityConfig.kt
@@ -0,0 +1,22 @@
+package com.cli.fancy.movie_reservation_system.api.user
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class UserSecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests { authorizeRequests ->
+            authorizeRequests.requestMatchers("/users").permitAll()
+                .anyRequest().authenticated()
+        }
+            .csrf{csrf -> csrf.disable()} // Disable CSRF
+            .formLogin{it.disable()} // Disable form login
+            .httpBasic{} // Enable basic authentication
+        return http.build()
+    }
+}
