c13781fab7296189e983ce4c2f88b6b27726817e
de-meyer <dmeyer.private@gmail.com>
2025-09-21 01:49:18 +0200

    fix: annotate repository and add open to main class

--- src/main/kotlin/com/cli/fancy/movie_reservation_system/MovieReservationSystemApplication.kt
+++ src/main/kotlin/com/cli/fancy/movie_reservation_system/MovieReservationSystemApplication.kt
@@ -1,13 +1,12 @@
 package com.cli.fancy.movie_reservation_system
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @EnableJpaRepositories
-class MovieReservationSystemApplication
+open class MovieReservationSystemApplication
 
 fun main(args: Array<String>) {
     runApplication<MovieReservationSystemApplication>(*args)
--- src/main/kotlin/com/cli/fancy/movie_reservation_system/infrastructure/persistence/user/UserRepository.kt
+++ src/main/kotlin/com/cli/fancy/movie_reservation_system/infrastructure/persistence/user/UserRepository.kt
@@ -1,9 +1,10 @@
 package com.cli.fancy.movie_reservation_system.infrastructure.persistence.user
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import java.util.*
 
-
+@Repository
 interface AuthRepository : JpaRepository<UserEntity, Long> {
     fun getUserEntityByName(name: String): UserEntity
     fun getUserByEmail(email: String): Optional<UserEntity>
