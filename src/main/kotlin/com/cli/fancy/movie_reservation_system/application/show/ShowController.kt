37894b4cec2482d90b3862dc87f2c04269e0ba6c
de-meyer <dmeyer.private@gmail.com>
2026-01-18 20:27:44 +0100

    refactor: WIP change to extension function

--- src/main/kotlin/com/cli/fancy/movie_reservation_system/application/show/ShowController.kt
+++ src/main/kotlin/com/cli/fancy/movie_reservation_system/application/show/ShowController.kt
@@ -3,31 +3,32 @@ package com.cli.fancy.movie_reservation_system.application.show
 import com.cli.fancy.movie_reservation_system.application.show.dto.ShowCreateRequest
 import com.cli.fancy.movie_reservation_system.application.show.dto.ShowIdRequest
 import com.cli.fancy.movie_reservation_system.application.show.dto.ShowResponse
-import com.cli.fancy.movie_reservation_system.application.show.mapper.ShowMapper
+import com.cli.fancy.movie_reservation_system.application.show.mapper.toEntity
+import com.cli.fancy.movie_reservation_system.application.show.mapper.toShowResponse
 import com.cli.fancy.movie_reservation_system.domain.show.ShowService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/show")
-class ShowController(val showService: ShowService, val showMapper: ShowMapper) {
+class ShowController(val showService: ShowService) {
 
     @GetMapping("/findAll")
     fun findAllShows(): ResponseEntity<List<ShowResponse>> {
-        val shows = showService.findAll().map { showMapper.toShowResponse(it) }
+        val shows = showService.findAll().map { it.toShowResponse() }
         return ResponseEntity.ok(shows)
     }
 
     @PostMapping("/find")
     fun findShowById(@RequestBody request: ShowIdRequest): ShowResponse {
-        val show = showMapper.toShowResponse(showService.findById(request.id))
+        val show = showService.findById(request.id).toShowResponse()
         return show
     }
 
     @PostMapping("/create")
     fun createShow(@RequestBody request: ShowCreateRequest): ResponseEntity<String> {
         try {
-            val showEntity = showMapper.toEntityFromCreateRequest(request)
+            val showEntity = request.toEntity()
             showService.createShow(showEntity = showEntity)
             return ResponseEntity.ok().body("Success")
         } catch (ex: Exception) {
--- src/main/kotlin/com/cli/fancy/movie_reservation_system/application/theater/mapper/TheaterMapper.kt
+++ src/main/kotlin/com/cli/fancy/movie_reservation_system/application/theater/mapper/TheaterMapper.kt
@@ -1,6 +1,5 @@
 package com.cli.fancy.movie_reservation_system.application.theater.mapper
 
-import com.cli.fancy.movie_reservation_system.application.theater.dto.TheaterRequest
 import com.cli.fancy.movie_reservation_system.application.theater.dto.TheaterResponse
 import com.cli.fancy.movie_reservation_system.domain.theater.Theater
 import com.cli.fancy.movie_reservation_system.infrastructure.persistence.theater.TheaterEntity
