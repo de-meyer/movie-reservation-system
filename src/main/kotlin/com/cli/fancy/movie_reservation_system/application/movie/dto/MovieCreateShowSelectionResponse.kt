18ae3c7b6ab8454180580cbd510a04e7e1605551
de-meyer <dmeyer.private@gmail.com>
2026-01-25 03:34:04 +0100

    refactor: remove unnecessary code

--- src/main/kotlin/com/cli/fancy/movie_reservation_system/application/movie/dto/MovieCreateShowSelectionResponse.kt
+++ src/main/kotlin/com/cli/fancy/movie_reservation_system/application/movie/dto/MovieCreateShowSelectionResponse.kt
@@ -6,24 +6,4 @@ data class MovieCreateShowSelectionResponse(
     val id: UUID,
     val title: String,
     val durationMinutes: Int,
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is MovieResponse) return false
-
-        if (durationMinutes != other.durationMinutes) return false
-        if (id != other.id) return false
-        if (title != other.title) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = durationMinutes
-        result = 31 * result + id.hashCode()
-        result = 31 * result + title.hashCode()
-        return result
-    }
-
-
-}
+)
--- src/main/kotlin/com/cli/fancy/movie_reservation_system/domain/movie/Movie.kt
+++ src/main/kotlin/com/cli/fancy/movie_reservation_system/domain/movie/Movie.kt
@@ -12,35 +12,4 @@ data class Movie(
     val releaseYear: Int,
     val imageProfile: String,
     val imageLandscape: String
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is Movie) return false
-
-        if (durationMinutes != other.durationMinutes) return false
-        if (releaseYear != other.releaseYear) return false
-        if (id != other.id) return false
-        if (title != other.title) return false
-        if (description != other.description) return false
-        if (director != other.director) return false
-        if (genre != other.genre) return false
-        if (imageProfile != other.imageProfile) return false
-        if (imageLandscape != other.imageLandscape) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = durationMinutes
-        result = 31 * result + releaseYear
-        result = 31 * result + (id.hashCode() ?: 0)
-        result = 31 * result + title.hashCode()
-        result = 31 * result + description.hashCode()
-        result = 31 * result + (director.hashCode() ?: 0)
-        result = 31 * result + (genre.hashCode() ?: 0)
-        result = 31 * result + (imageProfile.hashCode() ?: 0)
-        result = 31 * result + (imageLandscape.hashCode() ?: 0)
-        return result
-    }
-
-}
+)
